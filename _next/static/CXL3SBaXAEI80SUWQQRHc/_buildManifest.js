2c73fb764b36a4bcc427e523537d7bcb2efd79d1
Dasun110 <dasunpcc50@gmail.com>
2024-04-18 23:34:59 +0530

    update

---
+++ _next/static/CXL3SBaXAEI80SUWQQRHc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f76788cadc76491e.css","static/chunks/166-d659c72991712795.js","static/chunks/pages/index-0f993f562c1bcaf8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
