b199b4fc624f54c524ef45dbf0e5aa574acdd792
Dasun110 <dasunpcc50@gmail.com>
2024-04-18 22:20:16 +0530

    DasunSathsara-Portfolio

---
+++ out/_next/static/6nV1pnOCVtcC0Ia4RgkCB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f76788cadc76491e.css","static/chunks/166-d659c72991712795.js","static/chunks/pages/index-ddac43224fcc75ef.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
